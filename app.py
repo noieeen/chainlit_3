0f99fb1b858f817e115535be09484222a70f2cd9
noieeen <tharnnn1401@gmail.com>
2025-09-14 19:28:45 +0700

    init chat session

---
+++ app.py
@@ -0,0 +1,55 @@
+import os
+import chainlit as cl
+from chainlit.data.sql_alchemy import SQLAlchemyDataLayer
+
+
+db_conn = os.getenv("DATABASE_URL", "")
+
+
+@cl.data_layer
+def get_data_layer():
+    return SQLAlchemyDataLayer(conninfo=db_conn)
+
+
+@cl.password_auth_callback
+def auth_callback(username: str, password: str):
+    # Fetch the user matching username from your database
+    # and compare the hashed password with the value stored in the database
+    if (username, password) == ("admin", "admin"):
+        return cl.User(
+            identifier="admin", metadata={"role": "admin", "provider": "credentials"}
+        )
+    else:
+        return None
+
+@cl.on_chat_resume
+async def on_chat_resume(thread):
+    pass
+
+
+@cl.step(type="tool")
+async def tool():
+    # Fake tool
+    await cl.sleep(2)
+    return "Response from the tool!"
+
+
+
+
+@cl.on_message  # this function will be called every time a user inputs a message in the UI
+async def main(message: cl.Message):
+    """
+    This function is called every time a user inputs a message in the UI.
+    It sends back an intermediate response from the tool, followed by the final answer.
+
+    Args:
+        message: The user's message.
+
+    Returns:
+        None.
+    """
+
+    # Call the tool
+    tool_res = await tool()
+
+    await cl.Message(content=tool_res).send()
