8cbe169e9f9027f19e9cac3599958cfb542cb2e2
noieeen <tharnnn1401@gmail.com>
2025-09-16 19:03:25 +0700

    connect to S3 (upload working)

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 import os
 import chainlit as cl
 from chainlit.data.sql_alchemy import SQLAlchemyDataLayer
+# from chainlit.data.dynamodb import DynamoDBDataLayer
+from chainlit.data.storage_clients.s3 import S3StorageClient
 
 
 db_conn = os.getenv("DATABASE_URL", "")
+bucket = os.getenv("BUCKET_NAME", "")
+aws_access_key = os.getenv("APP_AWS_ACCESS_KEY", "")
+aws_secret_key = os.getenv("APP_AWS_SECRET_KEY", "")
+aws_region = os.getenv("APP_AWS_REGION", "")
+aws_endpoint = os.getenv("DEV_AWS_ENDPOINT", "")
+
+storage_client = S3StorageClient(
+    bucket=bucket,
+    aws_access_key_id=aws_access_key,
+    aws_secret_access_key=aws_secret_key,
+    region_name=aws_region,
+    endpoint_url=aws_endpoint
+)
 
 
 @cl.data_layer
 def get_data_layer():
-    return SQLAlchemyDataLayer(conninfo=db_conn)
+    # DynamoDBDataLayer(table_name="<Your Table>", storage_provider=storage_client)
+    return SQLAlchemyDataLayer(conninfo=db_conn, storage_provider=storage_client)
 
 
 @cl.password_auth_callback
